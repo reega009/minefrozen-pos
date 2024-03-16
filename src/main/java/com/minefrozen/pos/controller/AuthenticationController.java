b2f240448805946989f8e249637170d1572a9b6a
reega009 <reega009@gmail.com>
2024-03-16 14:43:14 +0700

    Update

--- src/main/java/com/minefrozen/pos/controller/AuthenticationController.java
+++ src/main/java/com/minefrozen/pos/controller/AuthenticationController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/pos/auth")
 @Slf4j
 public class AuthenticationController {
 
